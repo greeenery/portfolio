fcd08adb4dbe44f5690c4772c219a5bf40db29f0
greeenery <07_mz@naver.com>
2023-05-12 10:22:26 +0900

    230512 완성

--- mijinfood/js/script.js
+++ mijinfood/js/script.js
@@ -1,26 +1,57 @@
-const swiper = new Swiper('.main-slider .swiper', {
-  loop: true,
+function SliderBox1__init() {
+  const swiper = new Swiper('.main-slider .swiper', {
+    loop: true,
+    autoplay:true,
+    effect:'fade',
+    // If we need pagination
+    pagination: {
+      el: '.main-slider .swiper-pagination',
+    },
 
-  // If we need pagination
-  pagination: {
-    el: '.main-slider .swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.main-slider .swiper-button-next',
-    prevEl: '.main-slider .swiper-button-prev',
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.main-slider .swiper-button-next',
+      prevEl: '.main-slider .swiper-button-prev',
+    },
+  });
+}
 
 
-const swiper2 = new Swiper('.new-prod-slider .swiper', {
-  loop: true,
-  slidesPerView :3,
-  spaceBetween:48,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
-  },
-});
+function SliderBox2__init() {
+  const swiper = new Swiper('.new-prod-slider .swiper', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 48,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+  });
+}
+
+SliderBox1__init();
+SliderBox2__init();
+
+function Category__init(){
+  $(".btn-category").click(function(){
+    if($(".category-box").hasClass("active")){
+      $(".category-box").removeClass("active");
+      $(".hamburger").removeClass("active"); 
+    }
+    else {
+      $(".category-box").addClass("active");
+      $(".hamburger").addClass("active"); 
+    }
+  });
+}
+
+Category__init();
+
+function FloatingMenu__init(){
+  $(".btn-top").click(function(){
+    $("html, body").animate({scrollTop:"0"},300);
+  });
+}
+
+FloatingMenu__init();
